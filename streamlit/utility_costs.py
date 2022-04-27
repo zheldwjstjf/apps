2a3010cae480a9e8ba83f317abac6132d2b74ab6
zheldwjstjf <52637810+zheldwjstjf@users.noreply.github.com>
2022-04-28 02:32:40 +0900

    Update utility_costs.py

--- streamlit/utility_costs.py
+++ streamlit/utility_costs.py
@@ -31,7 +31,7 @@ st.header('我が家の光熱費')
 def get_utility_costs_data():
      url = "https://raw.github.com/zheldwjstjf/apps/blob/main/streamlit/data/utility_costs.csv"
      page = requests.get(url)
-     print page.text
+     print(page.text)
      df = pd.read_csv(page.text)
 
     st.sidebar.write(111)
