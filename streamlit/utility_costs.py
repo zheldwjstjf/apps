1f12b2f5be501f21ce1b04fa11e7f8e0692ed182
zheldwjstjf <52637810+zheldwjstjf@users.noreply.github.com>
2022-04-28 02:40:31 +0900

    Update utility_costs.py

--- streamlit/utility_costs.py
+++ streamlit/utility_costs.py
@@ -2,6 +2,7 @@ import streamlit as st
 import datetime
 import pandas as pd
 import altair as alt
+import base64
 import requests
 
 from urllib.error import URLError
@@ -27,8 +28,16 @@ else:
 # main page
 st.header('我が家の光熱費')
 
+url = 'https://api.github.com/repos/zheldwjstjf/apps/blob/main/streamlit/data/utility_costs.csv'
+req = requests.get(url)
+if req.status_code == requests.codes.ok:
+    req = req.json()  # the response is a JSON
+    # req is now a dict with keys: name, encoding, url, size ...
+    # and content. But it is encoded with base64.
+    content = base64.decodestring(req['content'])
+else:
+    print('Content was not found.')
+
 st.write(111)
-url = "https://raw.github.com/zheldwjstjf/apps/blob/main/streamlit/data/utility_costs.csv"
-page = requests.get(url)
-st.write(page.text)
+st.write(content)
 st.write(222)
