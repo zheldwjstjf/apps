8778ec90dbad9a635516a81cbc80f31df4a687e0
imjackicle <imjackicle@gmail.com>
2022-05-03 08:14:22 +0900

    Update main.py
    File "/app/apps/streamlit/Utility_costs/main.py", line 31, in <module>
        page_icon=None,  # String, anything supported by st.image, or None.

--- streamlit/Utility_costs/main.py
+++ streamlit/Utility_costs/main.py
@@ -4,23 +4,6 @@ from pages.main_page import MainPage
 from pages.side_menu import SideMenu
 from modules.csv_tool import CSVTool
 
-"""
-import datetime
-import pandas as pd
-import altair as alt
-import base64
-import requests
-import json
-from io import StringIO
-import os
-from apiclient.discovery import build
-import webbrowser
-from oauth2client.client import OAuth2WebServerFlow
-from oauth2client.file import Storage
-import httplib2
-from apiclient import errors
-"""
-
 # ===================================
 # st config
 # ===================================
@@ -28,7 +11,7 @@ st.set_page_config(  # Alternate names: setup_page, page, layout
     layout="wide",  # Can be "centered" or "wide". In the future also "dashboard", etc.
     initial_sidebar_state="expanded",  # Can be "auto", "expanded", "collapsed"
     page_title="我が家の光熱費",  # String or None. Strings get appended with "• Streamlit". 
-    page_icon=None,  # String, anything supported by st.image, or None.
+    # page_icon=None,  # String, anything supported by st.image, or None.
 )
 
 class App:
