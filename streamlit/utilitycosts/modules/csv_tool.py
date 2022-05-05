920a7400a1e751fdd8da777bed7594203160dd78
imjackicle <imjackicle@gmail.com>
2022-05-05 11:02:48 +0900

    Revert "일단은 로컬에서 등록한다...ㅠㅠ"
    This reverts commit 7065d7832dca54567f75b3800f79684d691178d4.

--- streamlit/utilitycosts/modules/csv_tool.py
+++ streamlit/utilitycosts/modules/csv_tool.py
@@ -46,7 +46,7 @@ class CSVTool:
         """
         save input
         """
-        # df.at[row, selected_date] = amount
-        # df.to_csv("data/utility_costs.csv", index=None)
+        df.at[row, selected_date] = amount
+        df.to_csv("data/utility_costs.csv", index=None)
         
         return None
