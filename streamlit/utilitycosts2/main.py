bf3d2fe841d1ae96380e66a3c4a6a16fa30e86db
imjackicle <imjackicle@gmail.com>
2022-05-07 13:14:27 +0900

    불필요한 메지시 프린트 삭제

--- streamlit/utilitycosts2/main.py
+++ streamlit/utilitycosts2/main.py
@@ -25,42 +25,19 @@ st.set_page_config(  # Alternate names: setup_page, page, layout
 class App:
 
     def __init__(self):
-
-        print("[DEBUG] 00001 - def __init__(self)")
-
         self.st = st
 
         #
-        print("[DEBUG] 00002 - def __init__(self)")
         self.sm = SideMenu(st)
         self.mp = MainPage(st)
         self.cSVTool = CSVTool(self.st)
 
-        #
-        print("[DEBUG] 00003 - def __init__(self)")
-        self.auth_url = "https://accounts.google.com/o/oauth2/auth?"
-        self.response_setting = {
-            "scope": "https://mail.google.com/",
-            "response_type": "code"}
-
-        #
-        print("[DEBUG] 00004 - def __init__(self)")
         self.auth_status = ""
-        self.auth_status2 = ""
-        self.code = ""
-        self.flow_step2 = ""
-
-        print("[DEBUG] 00005 - def __init__(self)")
-
 
 
     def main(self):
-
-        print("[DEBUG] 00006 - def main(self)")
-
         # =================
         # main page title
-        print("[DEBUG] 00007 - def main(self)")
         self.st.markdown("<h1 style='text-align: center; color: red;'>我が家の光熱費</h1>", unsafe_allow_html=True)
 
 
@@ -71,12 +48,11 @@ class App:
 
         # =================
         # side mmenu title
-        print("[DEBUG] 00008 - def main(self)")
         st.sidebar.markdown("<h1 style='text-align: center; color: red;'>[ S I D E - M E N U ]</h1>", unsafe_allow_html=True)
 
 
         # =================
-        print("[DEBUG] 00009 - def main(self)")
+
         self.auth_status = self.auth()
 
         if (self.auth_status != None) and (self.auth_status != False):
@@ -88,7 +64,6 @@ class App:
                 self.sm.side_menu(self.df)
 
         # =================
-        print("[DEBUG] 00010 - def main(self)")
         if (self.auth_status == None):
             st.sidebar.markdown("<h3 style='text-align: left; color: red;'>認証が必要です。</h3>", unsafe_allow_html=True)
 
@@ -98,10 +73,7 @@ class App:
 
 
     def auth(self):
-
         with st.sidebar.expander("[ 認証 ]"):
-
-            self.st.write("auth_page", "111>>> ", str(self.auth_status))
 
             self.st.markdown("<h3 style='text-align: left; color: red;'>認証キーを選択してください。</h3>", unsafe_allow_html=True)
 
@@ -115,14 +87,15 @@ class App:
                 auth_info = json.load(stringio)
 
                 if auth_info['installed']["client_id"] == st.secrets["_client_id"]:
+
                     return True
+
                 else:
                     self.st.write("idが一致しません。")
+
                     return False
 
             elif uploaded_file is None:
-                self.st.write("auth_page", "777>>> ", str(self.auth_status))
-                print("[DEBUG] auth_status - main.py - 777 : >>> ", str(self.auth_status))
                 self.st.write("認証キーが選択されてません。")
                     
                 return False
