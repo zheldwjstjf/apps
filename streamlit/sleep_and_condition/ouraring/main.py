82665483c5a659170e665b7f9066385d089529c9
imjackicle <imjackicle@gmail.com>
2022-05-14 13:22:22 +0900

    처음 화면 불러올 때 사이드바 닫은 상태가 되도록 수정

--- streamlit/sleep_and_condition/ouraring/main.py
+++ streamlit/sleep_and_condition/ouraring/main.py
@@ -43,7 +43,7 @@ def getSleepData(start_date):
 
 st.set_page_config( # Alternate names: setup_page, page, layout
     layout="wide",  # Can be "centered" or "wide". In the future also "dashboard", etc.
-    # initial_sidebar_state="collapsed",  # Can be "auto", "expanded", "collapsed"
+    initial_sidebar_state="collapsed",  # Can be "auto", "expanded", "collapsed"
     page_title="my_sleep_graph",  # String or None. Strings get appended with "• Streamlit". 
     # page_icon=None,  # String, anything supported by st.image, or None.)
 )
