0e5023553fa2b406e3e9c13ac1d1c4a998a98273
imjackicle <imjackicle@gmail.com>
2022-05-20 06:16:00 +0900

    수면 관련 시간들을 %로 표시5

--- streamlit/sleep_and_condition/ouraring/pages/main_page.py
+++ streamlit/sleep_and_condition/ouraring/pages/main_page.py
@@ -59,14 +59,14 @@ class MainPage:
             
             duration = sleep_dict.get("横になってた時間")
 
-            col1, col2, col3, col4 = self.st.columns((1,1,2,1))
+            col1, col2, col3, col4 = self.st.columns((1,1,1.5,1))
             col2.write(" ▶︎ スコア")
             for key_word in key_word_list1:
                 col2.write(" - " + str(key_word) + " : " + str(sleep_dict.get(key_word)))
 
             col3.write(" ▶︎ 時間（分）")
             for key_word in key_word_list2:
-                col3.write(" - " + str(key_word) + " : " + str(sleep_dict.get(key_word)/60) + " / " + str(int((sleep_dict.get(key_word)/duration)*100)) + "%")
+                col3.write(" - " + str(key_word) + " : " + str(sleep_dict.get(key_word)/60) + " (" + str(int((sleep_dict.get(key_word)/duration)*100)) + "%)")
 
             col4.write(" ▶︎ その他")
             for key_word in key_word_list3:
