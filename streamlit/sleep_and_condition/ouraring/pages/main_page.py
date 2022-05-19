f736b8a03f01f3d63f1b219588e2dbb37380145a
imjackicle <imjackicle@gmail.com>
2022-05-20 05:27:32 +0900

    그래프 표시 항목 선택메뉴를 그래프 바로 옆으로 이동4

--- streamlit/sleep_and_condition/ouraring/pages/main_page.py
+++ streamlit/sleep_and_condition/ouraring/pages/main_page.py
@@ -43,17 +43,17 @@ class MainPage:
             df = pd.read_json(sleep_str)
 
             # show graph
-            col1, col2 = self.st.columns((1,1))
+            col1, col2 = self.st.columns((2,8))
             options1 = col1.multiselect('▶︎ 項目を選択', key_word_list1, default="総合スコア")
             chart_data = pd.DataFrame(df, columns=options1)
             col2.line_chart(chart_data)
 
-            col3, col4 = self.st.columns((1,1))
+            col3, col4 = self.st.columns((2,8))
             options2 = col3.multiselect('▶︎ 項目を選択', key_word_list2, default="睡眠時間")
             chart_data = pd.DataFrame(df, columns=options2)
             col4.line_chart(chart_data)
 
-            col5, col6 = self.st.columns((1,1))
+            col5, col6 = self.st.columns((2,8))
             options3 = col5.multiselect('▶︎ 項目を選択', key_word_list3, default="temperature_deviation")
             chart_data = pd.DataFrame(df, columns=options3)
             col6.line_chart(chart_data)
@@ -71,12 +71,12 @@ class MainPage:
 
             col7.write(" ▶︎ スコア")
             for key_word in key_word_list1:
-                col4.write(" - " + str(key_word) + " : " + str(sleep_dict.get(key_word)))
+                col7.write(" - " + str(key_word) + " : " + str(sleep_dict.get(key_word)))
 
             col8.write(" ▶︎ 時間（分）")
             for key_word in key_word_list2:
-                col5.write(" - " + str(key_word) + " : " + str(sleep_dict.get(key_word)/60))
+                col8.write(" - " + str(key_word) + " : " + str(sleep_dict.get(key_word)/60))
 
             col9.write(" ▶︎ その他")
             for key_word in key_word_list3:
-                col6.write(" - " + str(key_word) + " : " + str(sleep_dict.get(key_word)))
+                col9.write(" - " + str(key_word) + " : " + str(sleep_dict.get(key_word)))
