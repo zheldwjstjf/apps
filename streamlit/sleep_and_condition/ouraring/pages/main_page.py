756f172ff6746d93d5a4990ee3827e13e89136f1
imjackicle <imjackicle@gmail.com>
2022-05-20 06:14:06 +0900

    수면 관련 시간들을 %로 표시3

--- streamlit/sleep_and_condition/ouraring/pages/main_page.py
+++ streamlit/sleep_and_condition/ouraring/pages/main_page.py
@@ -59,18 +59,18 @@ class MainPage:
             
             duration = sleep_dict.get("横になってた時間")
 
-            col1, col2, col3, col4, col5, col6 = self.st.columns((1,1,1,2,1,1))
+            col1, col2, col3, col4, col5, col6 = self.st.columns((1,1,2,1))
             col2.write(" ▶︎ スコア")
             for key_word in key_word_list1:
                 col2.write(" - " + str(key_word) + " : " + str(sleep_dict.get(key_word)))
 
-            col4.write(" ▶︎ 時間（分）")
+            col3.write(" ▶︎ 時間（分）")
             for key_word in key_word_list2:
-                col4.write(" - " + str(key_word) + " : " + str(sleep_dict.get(key_word)/60) + " / " + str(int((sleep_dict.get(key_word)/duration)*100)) + "%")
+                col3.write(" - " + str(key_word) + " : " + str(sleep_dict.get(key_word)/60) + " / " + str(int((sleep_dict.get(key_word)/duration)*100)) + "%")
 
-            col6.write(" ▶︎ その他")
+            col4.write(" ▶︎ その他")
             for key_word in key_word_list3:
-                col6.write(" - " + str(key_word) + " : " + str(sleep_dict.get(key_word)))
+                col4.write(" - " + str(key_word) + " : " + str(sleep_dict.get(key_word)))
 
             ##################
             # show graph
