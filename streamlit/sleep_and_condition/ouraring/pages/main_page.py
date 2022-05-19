946305115ebff10d8eb3e5f2e0c11e9c8d8ae995
imjackicle <imjackicle@gmail.com>
2022-05-20 05:22:52 +0900

    그래프 표시 항목 선택메뉴를 그래프 바로 옆으로 이동

--- streamlit/sleep_and_condition/ouraring/pages/main_page.py
+++ streamlit/sleep_and_condition/ouraring/pages/main_page.py
@@ -21,6 +21,7 @@ class MainPage:
             sleep = sleep["sleep"]
             sleep_str = str(sleep)
 
+            # replace english keywords with japaense keywords
             sleep_str = sleep_str.replace("'score_deep'", "'熟睡'")
             sleep_str = sleep_str.replace("'score_efficiency'", "'睡眠効率'")
             sleep_str = sleep_str.replace("'score_latency'", "'入眠潜時'")
@@ -28,8 +29,8 @@ class MainPage:
             sleep_str = sleep_str.replace("'score_total'", "'合計睡眠'")
             sleep_str = sleep_str.replace("'score'", "'総合スコア'")
 
-            sleep_str = sleep_str.replace("'total'", "'横になってた時間'")
-            sleep_str = sleep_str.replace("'duration'", "'睡眠時間'")
+            sleep_str = sleep_str.replace("'total'", "'睡眠時間'")
+            sleep_str = sleep_str.replace("'duration'", "'横になってた時間'")
             sleep_str = sleep_str.replace("'deep'", "'深い睡眠'")
             sleep_str = sleep_str.replace("'rem'", "'レム'")
             sleep_str = sleep_str.replace("'light'", "'浅眠'")
@@ -42,16 +43,20 @@ class MainPage:
             df = pd.read_json(sleep_str)
 
             # show graph
-            col1, col2, col3 = self.st.columns((1,1,1))
-
+            col1, col2 = self.st.columns((1,1))
+            options1 = col1.sidebar.multiselect('▶︎ 項目を選択', key_word_list1, default="総合スコア")
             chart_data = pd.DataFrame(df, columns=options1)
-            self.st.line_chart(chart_data)
+            col2.line_chart(chart_data)
 
+            col3, col4 = self.st.columns((1,1))
+            options2 = col3.sidebar.multiselect('▶︎ 項目を選択', key_word_list2, default="睡眠時間")
             chart_data = pd.DataFrame(df, columns=options2)
-            self.st.line_chart(chart_data)
+            col4.line_chart(chart_data)
 
+            col5, col6 = self.st.columns((1,1))
+            options3 = col5.sidebar.multiselect('▶︎ 項目を選択', key_word_list3, default="temperature_deviation")
             chart_data = pd.DataFrame(df, columns=options3)
-            self.st.line_chart(chart_data)
+            col6.line_chart(chart_data)
 
 
             # show data
@@ -62,16 +67,16 @@ class MainPage:
 
             self.st.write("**詳細データ** : " + str(sleep_dict.get("summary_date")) + "の朝のデータ")
 
-            col4, col5, col6 = self.st.columns((1,1,1))
+            col7, col8, col9 = self.st.columns((1,1,1))
 
-            col4.write(" ▶︎ スコア")
+            col7.write(" ▶︎ スコア")
             for key_word in key_word_list1:
                 col4.write(" - " + str(key_word) + " : " + str(sleep_dict.get(key_word)))
 
-            col5.write(" ▶︎ 時間（分）")
+            col8.write(" ▶︎ 時間（分）")
             for key_word in key_word_list2:
                 col5.write(" - " + str(key_word) + " : " + str(sleep_dict.get(key_word)/60))
 
-            col6.write(" ▶︎ その他")
+            col9.write(" ▶︎ その他")
             for key_word in key_word_list3:
                 col6.write(" - " + str(key_word) + " : " + str(sleep_dict.get(key_word)))
