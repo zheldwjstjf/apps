1fa1da103f501e3c85eaaf922700b2568bda1193
imjackicle <imjackicle@gmail.com>
2022-05-20 06:48:05 +0900

    데이터와 그래프 사이에 공백을 추가2

--- streamlit/sleep_and_condition/ouraring/pages/main_page.py
+++ streamlit/sleep_and_condition/ouraring/pages/main_page.py
@@ -76,9 +76,9 @@ class MainPage:
             # show graph
             ##################
 
-            self.st.empty()
+            c = self.st.empty()
 
-            self.st.markdown("<h2 style='text-align: left; color: red;'> グラフ : " + str(start_date) + " ~ " + str(end_date) + "</h2>", unsafe_allow_html=True)
+            c.markdown("<h2 style='text-align: left; color: red;'> グラフ : " + str(start_date) + " ~ " + str(end_date) + "</h2>", unsafe_allow_html=True)
 
             col1, col2 = self.st.columns((1.5,8.5))
             options1 = col1.multiselect('▶︎ 項目を選択', key_word_list1, default="総合スコア")
