2a298ffa606399c2d751c39d55d7d5a2598a8813
imjackicle <imjackicle@gmail.com>
2022-05-21 20:38:10 +0900

    Update main_page.py

--- streamlit/sleep_and_condition/ouraring/pages/main_page.py
+++ streamlit/sleep_and_condition/ouraring/pages/main_page.py
@@ -114,7 +114,7 @@ class MainPage:
             chart_data = pd.DataFrame(sleep_start)
             self.st.line_chart(chart_data)
 
-            self.st.plotly_chart(chart_data)
+            # self.st.plotly_chart(chart_data)
 
             self.st.area_chart(chart_data)
 
