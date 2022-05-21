fd87557409fef6c97fbc9a251b57fd88f3427f85
imjackicle <imjackicle@gmail.com>
2022-05-21 10:45:00 +0900

    sleep_start 그래프 추가 5

--- streamlit/sleep_and_condition/ouraring/pages/main_page.py
+++ streamlit/sleep_and_condition/ouraring/pages/main_page.py
@@ -101,6 +101,7 @@ class MainPage:
             self.st.line_chart(chart_data)
 
             chart_data = pd.DataFrame(sleep_start)
+            chart_data = chart_data.iloc[::-1]
             self.st.line_chart(chart_data)
 
 
