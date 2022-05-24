c49583b5db8aa41d7f3b6b5dbfb520b4d71e6802
imjackicle <imjackicle@gmail.com>
2022-05-25 06:49:01 +0900

    기본 틀 갖추기

--- streamlit/my_gmail_app/main.py
+++ streamlit/my_gmail_app/main.py
@@ -1,2 +1,24 @@
 import streamlit as st
 
+class MyGmailApp:
+
+    def __init__(self) -> None:
+        """
+        - method name : __init__
+        - arg(s) : None
+        """
+
+    def main(self):
+        """
+        - method name : main
+        - arg(s) : None
+        """
+
+        st.set_page_config( # Alternate names: setup_page, page, layout
+            layout="wide",  # Can be "centered" or "wide". In the future also "dashboard", etc.
+            initial_sidebar_state="collapsed",  # Can be "auto", "expanded", "collapsed"
+            page_title="my_sleep_graph",  # String or None. Strings get appended with "• Streamlit". 
+            page_icon=None)  # String, anything supported by st.image, or None.
+
+myGmailApp = MyGmailApp()
+myGmailApp.main()
