ae1d2bf4e87fe82f85b071f1631d64633d9716c3
imjackicle <imjackicle@gmail.com>
2022-05-25 06:55:16 +0900

    탭 타이틀/아이콘 추가

--- streamlit/my_gmail_app/main.py
+++ streamlit/my_gmail_app/main.py
@@ -17,8 +17,8 @@ class MyGmailApp:
         st.set_page_config( # Alternate names: setup_page, page, layout
             layout="wide",  # Can be "centered" or "wide". In the future also "dashboard", etc.
             initial_sidebar_state="collapsed",  # Can be "auto", "expanded", "collapsed"
-            page_title="my_sleep_graph",  # String or None. Strings get appended with "• Streamlit". 
-            page_icon=None)  # String, anything supported by st.image, or None.
+            page_title="MyGmailApp",  # String or None. Strings get appended with "• Streamlit". 
+            page_icon="resources/gmail_icon.icns")  # String, anything supported by st.image, or None.
 
 myGmailApp = MyGmailApp()
 myGmailApp.main()
