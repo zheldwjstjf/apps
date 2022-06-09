52284ce8f3122cfee3f2a88f8ba842df3f941866
imjackicle <imjackicle@gmail.com>
2022-06-10 07:31:31 +0900

    Update main.py

--- streamlit/my_gmail_app/main.py
+++ streamlit/my_gmail_app/main.py
@@ -54,7 +54,7 @@ class MyGmailApp:
         # main page
 
         if auth_status == None:
-            # self.st.markdown("<h1 style='text-align: center; color: red;'>NOT AUTHORIZED</h1>", unsafe_allow_html=True)
+            self.st.markdown("<h1 style='text-align: center; color: red;'>NOT AUTHORIZED</h1>", unsafe_allow_html=True)
             # self.st.markdown("![Alt Text](https://raw.githubusercontent.com/zheldwjstjf/apps/dev/streamlit/my_gmail_app/resources/digital_0_1.gif)")
             img="https://raw.githubusercontent.com/zheldwjstjf/apps/dev/streamlit/my_gmail_app/resources/digital_0_1.gif"
             self.st.image(img, width=1500)
