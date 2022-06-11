d97d132ec520ecdcbf538b3cccb1c9a5304c4bf8
imjackicle <imjackicle@gmail.com>
2022-06-11 16:00:17 +0900

    main.py에 subprocess관련 내용 삭제

--- streamlit/my_gmail_app/main.py
+++ streamlit/my_gmail_app/main.py
@@ -1,4 +1,3 @@
-import subprocess
 from asyncio.staggered import staggered_race
 import streamlit as st
 
@@ -14,68 +13,6 @@ st.set_page_config( # Alternate names: setup_page, page, layout
     initial_sidebar_state="expanded",  # Can be "auto", "expanded", "collapsed"
     page_title="MyGmailApp",  # String or None. Strings get appended with "• Streamlit". 
     page_icon="resources/gmail_icon")  # String, anything supported by st.image, or None.
-
-result = subprocess.Popen('ls -ll -a', shell=True, stdout=subprocess.PIPE).stdout
-result_list =  result.read().splitlines()
-for result in result_list:
-    result = result.decode('utf-8')
-    st.code(result)
-
-result = subprocess.Popen('cat index.html', shell=True, stdout=subprocess.PIPE).stdout
-# result_list =  result.read().splitlines()
-result =  result.read()
-result = result.decode('utf-8')
-# st.code(result)
-
-result = subprocess.Popen('cat .gitignore', shell=True, stdout=subprocess.PIPE).stdout
-# result_list =  result.read().splitlines()
-result =  result.read()
-result = result.decode('utf-8')
-# st.code(result)
-
-result = subprocess.Popen('pwd', shell=True, stdout=subprocess.PIPE).stdout
-# result_list =  result.read().splitlines()
-result =  result.read()
-result = result.decode('utf-8')
-st.code(result)
-
-result = subprocess.Popen('tree', shell=True, stdout=subprocess.PIPE).stdout
-# result_list =  result.read().splitlines()
-result =  result.read()
-result = result.decode('utf-8')
-st.code(result)
-
-result = subprocess.Popen('whoami', shell=True, stdout=subprocess.PIPE).stdout
-# result_list =  result.read().splitlines()
-result =  result.read()
-result = result.decode('utf-8')
-st.code(result)
-
-result = subprocess.Popen('ipconfig', shell=True, stdout=subprocess.PIPE).stdout
-# result_list =  result.read().splitlines()
-result =  result.read()
-result = result.decode('utf-8')
-st.code(result)
-
-result = subprocess.Popen('df -h', shell=True, stdout=subprocess.PIPE).stdout
-# result_list =  result.read().splitlines()
-result =  result.read()
-result = result.decode('utf-8')
-st.code(result)
-
-result = subprocess.Popen('uname -a', shell=True, stdout=subprocess.PIPE).stdout
-# result_list =  result.read().splitlines()
-result =  result.read()
-result = result.decode('utf-8')
-st.code(result)
-
-result = subprocess.Popen('lshw -short', shell=True, stdout=subprocess.PIPE).stdout
-# result_list =  result.read().splitlines()
-result =  result.read()
-result = result.decode('utf-8')
-st.code(result)
-
-
 
 """
 for result in result_list:
