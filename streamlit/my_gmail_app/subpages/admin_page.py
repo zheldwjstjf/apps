01a5193d70a4a45a47a7960deb313414c0eae7c4
imjackicle <imjackicle@gmail.com>
2022-06-12 11:13:09 +0900

    Revert "커맨드라인 블럭을 하나 더 추가 2
    placehonder, key를 추가

--- streamlit/my_gmail_app/subpages/admin_page.py
+++ streamlit/my_gmail_app/subpages/admin_page.py
@@ -24,7 +24,7 @@ class AdminPage:
         col1, col2 = self.st.columns((1,1))
         
         # input command line
-        commandLine = col1.text_input("Input Command Line")
+        commandLine = col1.text_input("Input Command Line", placeholder="Type command line here", key=1)
 
         if commandLine == None:
             pass
@@ -36,7 +36,7 @@ class AdminPage:
             col1.code(result)
 
         # input command line
-        commandLine = col2.text_input("Input Command Line")
+        commandLine = col2.text_input("Input Command Line", placeholder="Type command line here", key=2)
 
         if commandLine == None:
             pass
