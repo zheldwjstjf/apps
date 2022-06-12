439dc97509a093e9a0f7524429d9293c26b223bd
imjackicle <imjackicle@gmail.com>
2022-06-12 11:45:45 +0900

    로그 섹션 추가

--- streamlit/my_gmail_app/subpages/admin_page.py
+++ streamlit/my_gmail_app/subpages/admin_page.py
@@ -19,7 +19,7 @@ class AdminPage:
         # title
         self.st.markdown("<h1 style='text-align: center; color: red;'>Admin Page</h1>", unsafe_allow_html=True)
 
-        with self.st.expander("See explanation"):
+        with self.st.expander("Streamlit Server Terminal"):
 
             commandLine = None
             col1, col2 = self.st.columns((1,1))
@@ -43,10 +43,25 @@ class AdminPage:
                 pass
             else:
                 result = subprocess.Popen(commandLine, shell=True, stdout=subprocess.PIPE).stdout
-                # result_list =  result.read().splitlines()
                 result =  result.read()
                 result = result.decode('utf-8')
                 col2.code(result)
+
+
+        with self.st.expander("Streamlit Server Log"):
+
+            commandLine = "tail -100 /app/app.log"
+
+            if commandLine == None:
+                pass
+            else:
+                result = subprocess.Popen(commandLine, shell=True, stdout=subprocess.PIPE).stdout
+                result_list =  result.read().splitlines()
+                for result in result_list:
+                    result =  result.read()
+                    result = result.decode('utf-8')
+                    self.st.code(result)
+
 
 
         """
