692bdc6047c350e7f40cf841ee9337055302f5f4
imjackicle <imjackicle@gmail.com>
2022-06-13 02:27:02 +0900

    getMailList에서 디버깅 할 수 있게 함 2

--- streamlit/my_gmail_app/subpages/gmail_page.py
+++ streamlit/my_gmail_app/subpages/gmail_page.py
@@ -49,7 +49,7 @@ class GmailPage:
             self.get_list()
 
     def get_list(self):
-        maillist = self.gmail_api.getMailList(self.st, self.user, self.query)
+        maillist = self.gmail_api.getMailList(self.user, self.query)
         self.result_count = maillist['resultSizeEstimate']
         self.st.write("取得条件 : " + self.query)
         self.st.write("取得件数 : " + str(self.result_count) + " 件")
