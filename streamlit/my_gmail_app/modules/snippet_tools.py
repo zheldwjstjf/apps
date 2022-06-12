fa9d626a25f347124680e4a6d481be44acead622
imjackicle <imjackicle@gmail.com>
2022-06-12 12:59:14 +0900

    snippet_tools.py를 moduels 폴더로 이동

---
+++ streamlit/my_gmail_app/modules/snippet_tools.py
@@ -0,0 +1,21 @@
+class SnippetTools:
+
+    def __init__(self, streamlit) -> None:
+        self.st = streamlit
+
+    def image_alignment(self, img_path, img_width):
+        col1, col2, col3 = self.st.beta_columns([1,6,1])
+
+        with col1:
+            self.st.write("")
+
+        with col2:
+            #
+            img=img_path
+            self.st.image(img, width=img_width)
+            
+            #
+            # self.st.markdown("![Alt Text](img_path)")
+
+        with col3:
+            self.st.write("")
