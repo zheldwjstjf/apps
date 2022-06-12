fc9b4a201a39938e401c01b1bef8615171c40252
imjackicle <imjackicle@gmail.com>
2022-06-12 13:14:11 +0900

    columns((0.1, 8, 1))

--- streamlit/my_gmail_app/modules/snippet_tools.py
+++ streamlit/my_gmail_app/modules/snippet_tools.py
@@ -4,7 +4,7 @@ class SnippetTools:
         self.st = streamlit
 
     def image_alignment(self, img_path, img_width):
-        col1, col2, col3 = self.st.columns((0.3, 8, 1))
+        col1, col2, col3 = self.st.columns((0.1, 8, 1))
 
         with col1:
             self.st.write("")
