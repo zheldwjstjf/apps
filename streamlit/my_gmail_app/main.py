bdb2ac76da9cd131718c4b3ada178e27dfba9c69
imjackicle <imjackicle@gmail.com>
2022-06-16 03:08:31 +0900

    Update main.py

--- streamlit/my_gmail_app/main.py
+++ streamlit/my_gmail_app/main.py
@@ -57,6 +57,9 @@ class MyGmailApp:
             else:
                 self.st.error("認証失敗")
 
+            self.st.sidebar.info("[ TODO ] 認証した時刻")
+            self.st.sidebar.info("[ TODO ] 認証切れた時刻")
+
         # =================
         # main page
 
