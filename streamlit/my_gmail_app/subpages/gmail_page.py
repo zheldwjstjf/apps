e5570a8e6b5cd555225a66a5341580641ea8a22d
imjackicle <imjackicle@gmail.com>
2022-06-17 05:45:41 +0900

    Update gmail_page.py

--- streamlit/my_gmail_app/subpages/gmail_page.py
+++ streamlit/my_gmail_app/subpages/gmail_page.py
@@ -325,12 +325,12 @@ class GmailPage:
                 self.st.write("---")
             
             # TODO
-            """
+            self.st.write("""
             여기에 
             해당 메일에서 지금까지 보내온 메일의 
             본문 텍스트에 출현한 키워드의 
             출현 빈도 시각화 이미지를 뿌려 줄 것
-            """
+            """)
 
             col1, col2 = self.st.columns((1,1))
 
