a26a15fbfe7d8f4286c9198fee1e671850f7a493
imjackicle <imjackicle@gmail.com>
2022-06-18 01:33:50 +0900

    Update visualization_tool.py

--- streamlit/my_gmail_app/modules/visualization_tool.py
+++ streamlit/my_gmail_app/modules/visualization_tool.py
@@ -38,7 +38,7 @@ class VisualizationTool:
             self.st.error("Exception : " + str(e))
 
         try:
-            self.st.image(img_file_path, width=500)
+            self.snippetTools.image_alignment(img_file_path, 1000)
         except Exception as e:
             self.st.error("Exception : " + str(e))
 
