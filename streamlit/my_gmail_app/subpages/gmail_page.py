8b4a727c94c95eeb5f1c44bd0f469d960dd6dbd2
imjackicle <imjackicle@gmail.com>
2022-06-18 08:28:29 +0900

    Update gmail_page.py

--- streamlit/my_gmail_app/subpages/gmail_page.py
+++ streamlit/my_gmail_app/subpages/gmail_page.py
@@ -327,35 +327,35 @@ class GmailPage:
 
             
             # wordcloud
-            col5, col6 = self.st.columns((1,1))
+            col5, col6, col7 = self.st.columns((5,1,5))
 
             # wordcloud - mail_from
             with col5:
                 self.st.write("---")
                 self.st.write("● mail_from")
-                self.visualizationTool.wordcloud(mail_from, 450)
+                self.visualizationTool.wordcloud(mail_from, 700)
 
             # wordcloud - mail_subject
-            with col6:
+            with col7:
                 self.st.write("---")
                 self.st.write("● mail_subject")
-                self.visualizationTool.wordcloud(mail_subject, 550)
+                self.visualizationTool.wordcloud(mail_subject, 700)
 
             # wordcloud - mail_snippet
             with col5:
                 self.st.write("---")
                 self.st.write("● mail_snippet")
-                self.visualizationTool.wordcloud(mail_snippet, 450)
+                self.visualizationTool.wordcloud(mail_snippet, 700)
 
             # wordcloud - mail_body
-            with col6:
+            with col7:
                 self.st.write("---")
                 self.st.write("● mail_body")
                 if ("http" not in mail_body) and ("</" not in mail_body):
                     input_text = mail_body
                 else:
                     input_text = mail_body
-                self.visualizationTool.wordcloud(input_text, 550)
+                self.visualizationTool.wordcloud(input_text, 700)
 
 
 
