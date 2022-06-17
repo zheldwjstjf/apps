388c0643532a76c69fa6ba9b6e6b4f98ef589b45
imjackicle <imjackicle@gmail.com>
2022-06-18 02:12:24 +0900

    Update visualization_tool.py

--- streamlit/my_gmail_app/modules/visualization_tool.py
+++ streamlit/my_gmail_app/modules/visualization_tool.py
@@ -14,7 +14,7 @@ class VisualizationTool:
     def wordcloud(self, mail_body_text):
 
         stopwords = STOPWORDS
-        wc = WordCloud(stopwords=stopwords, background_color="white", max_words=30).generate(mail_body_text)
+        wc = WordCloud(stopwords=stopwords, background_color="white", max_words=77).generate(mail_body_text)
 
         img_folder_path = "/app/apps/images/"
         # Check whether the specified path exists or not
