78e74838317c5fec6e578e0df79a601e6577b2b0
imjackicle <imjackicle@gmail.com>
2022-06-18 15:49:03 +0900

    Update gmail_page.py

--- streamlit/my_gmail_app/subpages/gmail_page.py
+++ streamlit/my_gmail_app/subpages/gmail_page.py
@@ -409,6 +409,7 @@ class GmailPage:
             # moveMailToTrash
             with col11:
                 if self.st.button("🗑", key="trash_" + self.mail_id):
+                    self.st.balloons()
                     self.st.write("ゴミ箱に移動しました。")
                     pass
 
