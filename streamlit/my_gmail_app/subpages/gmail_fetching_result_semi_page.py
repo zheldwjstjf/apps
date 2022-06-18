783ea71535bbedb70c0820d6f3970753aa22872f
imjackicle <imjackicle@gmail.com>
2022-06-19 05:32:33 +0900

    contents_list, content_info 추가

--- streamlit/my_gmail_app/subpages/gmail_fetching_result_semi_page.py
+++ streamlit/my_gmail_app/subpages/gmail_fetching_result_semi_page.py
@@ -28,6 +28,9 @@ class GmailFetchingResultSemiPage:
 
         self.mail_id = None
 
+        self.contents_list = []
+        self.content_info = []
+
     def get_mail_content(self, maillist, fetching_count, result_count, service, user):
 
         self.gmail_api = GmailApi(self.st, service)
@@ -53,6 +56,8 @@ class GmailFetchingResultSemiPage:
                 self.mail_id = self.mail_id = maillist[i]['id']
                 self.mail_content = self.gmail_api.getMailContent(user, self.mail_id)
 
+                self.content_info.append(self.mail_id)
+
                 # - do mail as read
                 # self.gmail_api.markMailAsRead(user, self.mail_id)            
 
@@ -60,37 +65,45 @@ class GmailFetchingResultSemiPage:
 
                 try:
                     mail_subject = mail['subject']
+                    self.content_info.append(mail_subject)
                 except Exception as e:
                     self.st.error("Exception- mail['subject'] : " + "e")
 
                 try:
                     mail_date = mail['date']
+                    self.content_info.append(mail_date)
                 except Exception as e:
                     self.st.error("Exception- mail['date'] : " + "e")
 
                 try:
                     mail_from = mail['from']
+                    self.content_info.append(mail_from)
                 except Exception as e:
                     self.st.error("Exception- mail['from'] : " + "e")
 
                 try:
                     mail_to = mail['to']
+                    self.content_info.append(mail_to)
                 except Exception as e:
                     self.st.error("Exception- mail['to'] : " + "e")
 
                 try:
                     mail_snippet = mail['snippet']
+                    self.content_info.append(mail_snippet)
                 except Exception as e:
                     self.st.error("Exception- mail['snippet'] : " + "e")
 
                 try:
                     mail_body = mail['body']
+                    self.content_info.append(mail_body)
                 except Exception as e:
                     self.st.error("Exception- mail['body'] : " + "e")
 
                 for foo in range(3):
                     self.st.write("---")
                 self.st.subheader("▶︎ " + str(i+1) + " 件目")
+
+                self.contents_list.append(self.content_info)
 
                 ########################
 
@@ -131,6 +144,7 @@ class GmailFetchingResultSemiPage:
             except Exception as e:
                 self.st.warning("No more Email")
 
+        return self.contents_list
 
 
     def parse_mail(self):
