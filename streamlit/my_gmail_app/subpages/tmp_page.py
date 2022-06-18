8e95baf03187d269efbad96dc741c8992188ffa1
imjackicle <imjackicle@gmail.com>
2022-06-18 19:07:10 +0900

    Update tmp_page.py

--- streamlit/my_gmail_app/subpages/tmp_page.py
+++ streamlit/my_gmail_app/subpages/tmp_page.py
@@ -18,35 +18,41 @@ class TmpPage:
         """
 
         self.mail_id = mail_id
+        self.st.write("mail_id : ", self.mail_id)
 
         col11, col12, col13, col14, col15, col16, col17, col18, col19, col20 = self.st.columns((1,1,1,1,1,2,2,2,2,2))
 
         # moveMailToTrash
         with col11:
             if self.st.button("🗑", key="trash"): # _" + self.mail_id):
+                self.st.write("col11")
                 self.st.balloons()
                 self.st.write("ゴミ箱に移動しました。")
 
         # deleteMail
         with col12:
             if self.st.button("削除", key="delete_" + self.mail_id):
+                self.st.write("col12")
                 self.st.balloons()
                 self.st.write("削除しました。")
 
         # markMailAsImportant
         with col13:
             if self.st.button("重要", key="important_" + self.mail_id):
+                self.st.write("col13")
                 self.st.balloons()
                 self.st.write("重要なメールに指定しました。")
 
         # markMailAsStarred
         with col14:
             if self.st.button("⭐️", key="starred_" + self.mail_id):
+                self.st.write("col14")
                 self.st.balloons()
                 self.st.write("星を付けました。")
 
         # markMailAsUnread
         with col15:
             if self.st.button("未読", key="unread_" + self.mail_id):
+                self.st.write("col15")
                 self.st.balloons()
                 self.st.write("メールを未読に変更しました。")
