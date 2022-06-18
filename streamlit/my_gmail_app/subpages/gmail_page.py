77e59e6fb61a69984fdfb029db10bdd63e611ed4
imjackicle <imjackicle@gmail.com>
2022-06-18 14:47:42 +0900

    Update gmail_page.py

--- streamlit/my_gmail_app/subpages/gmail_page.py
+++ streamlit/my_gmail_app/subpages/gmail_page.py
@@ -363,7 +363,7 @@ class GmailPage:
             # getFilterList
             filter_dict = self.gmail_api.getFilterList(self.user, self.mail_id)
             self.st.write("filter_dict type : " + str(type(filter_dict)))
-            self.st.info(filter_dict)
+            # self.st.info("[DEBUG] filter_dict : " + str(filter_dict))
 
             try:
                 email_address = mail_from.split("@")[1]
