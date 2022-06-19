3b76966b5687720139c7fae064627f4b0d1daa4f
imjackicle <imjackicle@gmail.com>
2022-06-19 11:21:26 +0900

    Update gmail_fetching_result_full_page.py

--- streamlit/my_gmail_app/subpages/gmail_fetching_result_full_page.py
+++ streamlit/my_gmail_app/subpages/gmail_fetching_result_full_page.py
@@ -74,10 +74,10 @@ class GmailFetchingResultFullPage:
             except Exception as e:
                 self.st.error("Exception- mail['body'] : " + "e")
 
-            self.st.write("---")
-            self.st.subheader("▶︎ " + str(i+1) + " 件目")
             
-            """
+            for foo in range(3):
+                self.st.write("---")
+            
             # getFilterList
             filter_dict = self.gmail_api.getFilterList(user, self.mail_id)
             # self.st.info("[DEBUG] filter_dict : " + str(filter_dict))
@@ -100,7 +100,6 @@ class GmailFetchingResultFullPage:
                         self.st.info("Filter : " + str(filtered_email) + " : " + str(filter_action))
             else:
                 self.st.info("No Filter")
-            """
 
             # text
             col1, col2 = self.st.columns((1,1))
