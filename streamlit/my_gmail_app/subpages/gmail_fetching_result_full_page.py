24e6e219de6c6962e9418df163fd3df09ab2c0a8
imjackicle <imjackicle@gmail.com>
2022-06-25 02:27:53 +0900

    Update gmail_fetching_result_full_page.py

--- streamlit/my_gmail_app/subpages/gmail_fetching_result_full_page.py
+++ streamlit/my_gmail_app/subpages/gmail_fetching_result_full_page.py
@@ -116,8 +116,18 @@ class GmailFetchingResultFullPage:
                 self.st.error(str(e))
 
             try:
+                resutl_url_text_list_list = []
+                resutl_url_text_list = []
                 for target_url in target_urls:
                     resutl_text = self.getTextFromURL.extract_text_from_single_web_page(url=target_url)
+                    resutl_url_text_list.append(target_url)
+                    resutl_url_text_list.append(resutl_text)
+                    resutl_url_text_list_list.append(resutl_url_text_list)
+                resutl_url_text_list_list = list(set(resutl_url_text_list_list))
+
+                for resutl_url_text_list in resutl_url_text_list_list:
+                    target_url = resutl_url_text_list[0]
+                    resutl_text = resutl_url_text_list[1]
                     self.st.write("---")
                     self.st.write("● URL : " + target_url)
                     self.visualizationTool.wordcloud(resutl_text, 1400, 500)
