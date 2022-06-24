09d73d89ff233770ad9e6cc12f8bf07c2a9c3d9a
imjackicle <imjackicle@gmail.com>
2022-06-24 23:02:47 +0900

    Update gmail_fetching_result_semi_page.py

--- streamlit/my_gmail_app/subpages/gmail_fetching_result_semi_page.py
+++ streamlit/my_gmail_app/subpages/gmail_fetching_result_semi_page.py
@@ -127,19 +127,19 @@ class GmailFetchingResultSemiPage:
                 with col5:
                     self.st.write("---")
                     self.st.write("● mail_from")
-                    self.visualizationTool.wordcloud(mail_from, 700, 30)
+                    self.visualizationTool.wordcloud(mail_from, 600, 30)
 
                 # wordcloud - mail_subject
                 with col6:
                     self.st.write("---")
                     self.st.write("● mail_subject")
-                    self.visualizationTool.wordcloud(mail_subject, 700, 30)
+                    self.visualizationTool.wordcloud(mail_subject, 600, 30)
 
                 # wordcloud - mail_snippet
                 with col7:
                     self.st.write("---")
                     self.st.write("● mail_snippet")
-                    self.visualizationTool.wordcloud(mail_snippet, 700, 200)
+                    self.visualizationTool.wordcloud(mail_snippet, 600, 200)
 
                 # wordcloud - mail_body
                 self.st.write("---")
@@ -148,7 +148,7 @@ class GmailFetchingResultSemiPage:
                     input_text = mail_body
                 else:
                     input_text = mail_body
-                self.visualizationTool.wordcloud(input_text, 1800, 300)
+                self.visualizationTool.wordcloud(input_text, 1500, 300)
 
             except Exception as e:
                 self.st.warning("No more Email")
