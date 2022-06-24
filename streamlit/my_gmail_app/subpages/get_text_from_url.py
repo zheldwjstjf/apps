90fb84975d847915165109a8f7daa147535c1dc5
imjackicle <imjackicle@gmail.com>
2022-06-25 00:38:09 +0900

    url로부터  html다 때고 텍스트만 가져오기

--- streamlit/my_gmail_app/subpages/get_text_from_url.py
+++ streamlit/my_gmail_app/subpages/get_text_from_url.py
@@ -1,4 +1,10 @@
-import urllib2
+import json
+import requests
+from requests.models import MissingSchema
+from bs4 import BeautifulSoup
+import numpy as np
+import spacy
+import trafilatura
 
 class GetTextFromURL:
     """
@@ -15,12 +21,65 @@ class GetTextFromURL:
 
         self.result_text = ""
 
-    def get_text_from_url(self, target_url):
-
-        file = urllib2.urlopen(target_url).read(20000)
-
-        for line in file:
-            decoded_line = line.decode("utf-8")
-            self.result_text = self.result_text + decoded_line
+    def beautifulsoup_extract_text_fallback(self, response_content):
         
-        return self.result_text
+        '''
+        This is a fallback function, so that we can always return a value for text content.
+        Even for when both Trafilatura and BeautifulSoup are unable to extract the text from a 
+        single URL.
+        '''
+        
+        # Create the beautifulsoup object:
+        soup = BeautifulSoup(response_content, 'html.parser')
+        
+        # Finding the text:
+        text = soup.find_all(text=True)
+        
+        # Remove unwanted tag elements:
+        cleaned_text = ''
+        blacklist = [
+            '[document]',
+            'noscript',
+            'header',
+            'html',
+            'meta',
+            'head', 
+            'input',
+            'script',
+            'style',]
+
+        # Then we will loop over every item in the extract text and make sure that the beautifulsoup4 tag
+        # is NOT in the blacklist
+        for item in text:
+            if item.parent.name not in blacklist:
+                cleaned_text += '{} '.format(item)
+                
+        # Remove any tab separation and strip the text:
+        cleaned_text = cleaned_text.replace('\t', '')
+        return cleaned_text.strip()
+        
+
+    def get_text_from_url(self, url):
+        
+        downloaded_url = trafilatura.fetch_url(url)
+        try:
+            a = trafilatura.extract(downloaded_url, output_format="json", with_metadata=True, include_comments = False,
+                                date_extraction_params={'extensive_search': True, 'original_date': True})
+        except AttributeError:
+            a = trafilatura.extract(downloaded_url, output_format="json", with_metadata=True,
+                                date_extraction_params={'extensive_search': True, 'original_date': True})
+        if a:
+            json_output = json.loads(a)
+            return json_output['text']
+        else:
+            try:
+                resp = requests.get(url)
+                # We will only extract the text from successful requests:
+                if resp.status_code == 200:
+                    return self.beautifulsoup_extract_text_fallback(resp.content)
+                else:
+                    # This line will handle for any failures in both the Trafilature and BeautifulSoup4 functions:
+                    return np.nan
+            # Handling for any URLs that don't have the correct protocol
+            except MissingSchema:
+                return np.nan
