7e2ecbe7b007c8ee9d41a086ede1f1f97a13488b
imjackicle <imjackicle@gmail.com>
2022-06-24 23:04:31 +0900

    사이드바 폭 변경 2

--- streamlit/my_gmail_app/subpages/sidebar.py
+++ streamlit/my_gmail_app/subpages/sidebar.py
@@ -24,11 +24,11 @@ class SidebarPage:
             """
             <style>
             [data-testid="stSidebar"][aria-expanded="true"] > div:first-child {
-                width: 399px;
+                width: 386px;
             }
             [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {
-                width: 399;
-                margin-left: -399px;
+                width: 386;
+                margin-left: -386px;
             }
             </style>
             """,
