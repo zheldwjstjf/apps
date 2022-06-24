0edfbbe4e871e041c1a33471bd4412329f50a079
imjackicle <imjackicle@gmail.com>
2022-06-24 23:22:31 +0900

    Update gmail_fetching_result_semi_page.py

--- streamlit/my_gmail_app/subpages/gmail_fetching_result_semi_page.py
+++ streamlit/my_gmail_app/subpages/gmail_fetching_result_semi_page.py
@@ -133,13 +133,13 @@ class GmailFetchingResultSemiPage:
                 with col7:
                     self.st.write("---")
                     self.st.write("● mail_subject")
-                    self.visualizationTool.wordcloud(mail_subject, 350, 30)
+                    self.visualizationTool.wordcloud(mail_subject, 360, 30)
 
                 # wordcloud - mail_snippet
                 with col9:
                     self.st.write("---")
                     self.st.write("● mail_snippet")
-                    self.visualizationTool.wordcloud(mail_snippet, 600, 300)
+                    self.visualizationTool.wordcloud(mail_snippet, 630, 300)
 
                 # wordcloud - mail_body
                 self.st.write("---")
