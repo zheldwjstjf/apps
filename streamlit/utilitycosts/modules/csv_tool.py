c16c284c904e5e9b07881b4f43c031b43d52462c
imjackicle <imjackicle@gmail.com>
2022-06-26 02:10:43 +0900

    Update csv_tool.py

--- streamlit/utilitycosts/modules/csv_tool.py
+++ streamlit/utilitycosts/modules/csv_tool.py
@@ -14,7 +14,7 @@ class CSVTool:
 
         current_path = os.getcwd()
         self.st.info("current_path : " + current_path)
-        f = open(self.csv_data_file, "a+")
+        f = open(self.csv_data_file, "w")
         f.write("111")
         f.close() 
 
