5e9df80f6055a5de89408705d7b6bac7289fdc49
imjackicle <imjackicle@gmail.com>
2022-06-26 01:57:34 +0900

    Update csv_tool.py

--- streamlit/utilitycosts/modules/csv_tool.py
+++ streamlit/utilitycosts/modules/csv_tool.py
@@ -20,7 +20,7 @@ class CSVTool:
 
         self.url = "https://api.github.com/repos/zheldwjstjf/apps/contents/streamlit/utilitycosts/data/utility_costs.csv"
     
-    @st.cache(suppress_st_warning=True)
+    # @st.cache(suppress_st_warning=True)
     def load_data(self):
         """
         load spreadsheet with data to be annotated
