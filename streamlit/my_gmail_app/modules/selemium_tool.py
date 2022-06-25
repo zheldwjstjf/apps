f7c043647f4b8d14f37effd9096c9436f57eb207
imjackicle <imjackicle@gmail.com>
2022-06-26 00:46:24 +0900

    Update selemium_tool.py

--- streamlit/my_gmail_app/modules/selemium_tool.py
+++ streamlit/my_gmail_app/modules/selemium_tool.py
@@ -1,6 +1,40 @@
 import os
 import sys
 import streamlit as st
+
+class SeleniumTool:
+    def __init__(self, streamlit) -> None:
+        self.st = streamlit
+
+    @st.experimental_singleton
+    def installff():
+        os.system('sbase install geckodriver')
+        os.system('ln -s /home/appuser/venv/lib/python3.9/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
+
+    
+    _ = installff()
+    
+    from selenium import webdriver
+    from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+    from selenium.webdriver import FirefoxOptions
+    opts = FirefoxOptions()
+    opts.add_argument("--headless")
+    
+    try:
+        browser = webdriver.Firefox(executable_path="/home/appuser/venv/lib/python3.9/site-packages/seleniumbase/drivers/geckodriver", options=opts)
+    except Exception as e:
+        st.error(e)
+
+    """
+    browser.get('https://www.python.org/')
+    st.write(browser.page_source)
+    """
+
+
+"""
+import os
+import sys
+import streamlit as st
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
@@ -23,8 +57,7 @@ class SeleniumTool:
         firefoxOptions = Options()
         firefoxOptions.add_argument("--headless")
         service = Service(GeckoDriverManager().install())
-        # driver = webdriver.Firefox(options=firefoxOptions, service=service)
-        driver = webdriver.Firefox(executable_path="/home/appuser/.wdm/drivers/geckodriver/linux64/v0.31.0/geckodriver", options=firefoxOptions)
+        driver = webdriver.Firefox(options=firefoxOptions, service=service)
         
         driver.get(URL)
-
+"""
