85daaf03d14ad943e16fdd664d39b7cbeaed9835
imjackicle <imjackicle@gmail.com>
2022-06-25 23:53:21 +0900

    Update selemium_tool.py

--- streamlit/my_gmail_app/modules/selemium_tool.py
+++ streamlit/my_gmail_app/modules/selemium_tool.py
@@ -9,7 +9,7 @@ class SeleniumTool:
     @st.experimental_singleton
     def installff():
         os.system('sbase install geckodriver')
-        os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
+        os.system('ln -s /home/appuser/venv/lib/python3.9/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
 
     _ = installff()
     from selenium import webdriver
