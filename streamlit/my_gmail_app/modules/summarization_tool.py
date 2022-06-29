f9bfe3b0b9b9369704136dd8d81794d581221956
imjackicle <imjackicle@gmail.com>
2022-06-30 01:15:05 +0900

    Update summarization_tool.py

--- streamlit/my_gmail_app/modules/summarization_tool.py
+++ streamlit/my_gmail_app/modules/summarization_tool.py
@@ -69,26 +69,32 @@ class SummarizationTool:
         sentences = []
         summarize_text = []
 
-        sentences_list_tmp = sentences_text.splitlines()
-        for line in sentences_list_tmp:
-            if len(line) > 0:
-                sentences.append(line)
-        print(sentences)
+        try:
+            sentences_list_tmp = sentences_text.splitlines()
 
-        # Step 2 - Generate Similary Martix across sentences
-        sentence_similarity_martix = self.build_similarity_matrix(sentences, stop_words)
+            for line in sentences_list_tmp:
+                if len(line) > 0:
+                    sentences.append(line)
+            print(sentences)
 
-        # Step 3 - Rank sentences in similarity martix
-        sentence_similarity_graph = nx.from_numpy_array(sentence_similarity_martix)
-        scores = nx.pagerank(sentence_similarity_graph)
+            # Step 2 - Generate Similary Martix across sentences
+            sentence_similarity_martix = self.build_similarity_matrix(sentences, stop_words)
 
-        # Step 4 - Sort the rank and pick top sentences
-        ranked_sentence = sorted(((scores[i],s) for i,s in enumerate(sentences)), reverse=True)    
-        print("Indexes of top ranked_sentence order are ", ranked_sentence)    
+            # Step 3 - Rank sentences in similarity martix
+            sentence_similarity_graph = nx.from_numpy_array(sentence_similarity_martix)
+            scores = nx.pagerank(sentence_similarity_graph)
 
-        for i in range(top_n):
-            summarize_text.append(" ".join(ranked_sentence[i][1]))
+            # Step 4 - Sort the rank and pick top sentences
+            ranked_sentence = sorted(((scores[i],s) for i,s in enumerate(sentences)), reverse=True)    
+            print("Indexes of top ranked_sentence order are ", ranked_sentence)    
 
-        # Step 5 - Offcourse, output the summarize texr
-        # print("Summarize Text: \n", ". ".join(summarize_text))
-        self.st.write("Summarize Text: \n", ". ".join(summarize_text))
+            for i in range(top_n):
+                summarize_text.append(" ".join(ranked_sentence[i][1]))
+
+            # Step 5 - Offcourse, output the summarize texr
+            # print("Summarize Text: \n", ". ".join(summarize_text))
+            self.st.write("Summarize Text: \n", ". ".join(summarize_text))
+
+        except Exception as e:
+            self.st.write("No Result")
+
