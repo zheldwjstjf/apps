2b766ef69a96e92a70039eefdcabf2808a96fecd
imjackicle <imjackicle@gmail.com>
2022-06-30 01:07:07 +0900

    Update summarization_tool.py

--- streamlit/my_gmail_app/modules/summarization_tool.py
+++ streamlit/my_gmail_app/modules/summarization_tool.py
@@ -63,7 +63,7 @@ class SummarizationTool:
         return similarity_matrix
 
 
-    def generate_summary(self, sentences_text, top_n=5):
+    def generate_summary(self, sentences_text, top_n=1):
         stop_words = stopwords.words('english')
 
         sentences = []
