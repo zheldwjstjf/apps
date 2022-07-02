4939fb4811a48648ecab696006e50309fdc8bcb8
imjackicle <imjackicle@gmail.com>
2022-07-03 01:09:45 +0900

    메일 필터 설정에서 조건에 맞는 메일 가져오기 건수 초기값을 1에서 100으로 변경

--- streamlit/my_gmail_app/subpages/gmail_fetching_setting_page.py
+++ streamlit/my_gmail_app/subpages/gmail_fetching_setting_page.py
@@ -70,7 +70,7 @@ class GmailFetchingSettingPage:
         self.st.code("Query : " + self.query)
 
         # select fetch count
-        self.fetching_count = int(self.st.number_input("● 最大Match 件数 (上限1000)", min_value=1, max_value=1000))
+        self.fetching_count = int(self.st.number_input("● 最大Match 件数 (上限1000)", min_value=100, max_value=1000))
 
     def get_priority_label(self):
 
